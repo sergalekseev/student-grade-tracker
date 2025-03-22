62e7c3cc24375e7cc5b6afab24d24446954ad51a
Sergei Alekseev <sergey.alekseev@raftds.com>
2025-03-22 12:22:16 +0300

    control implementation

---
+++ Components/StudentItemControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using StudentGradeTracker.Models;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace StudentGradeTracker.Components
+{
+    /// <summary>
+    /// Interaction logic for StudentItemControl.xaml
+    /// </summary>
+    public partial class StudentItemControl : UserControl
+    {
+        public static readonly DependencyProperty StudentProperty =
+            DependencyProperty.Register(
+                nameof(Student), 
+                typeof(Student), 
+                typeof(StudentItemControl));
+
+        public static readonly DependencyProperty RemoveStudentCommandProperty =
+            DependencyProperty.Register(
+                nameof(RemoveStudentCommand), 
+                typeof(ICommand), 
+                typeof(StudentItemControl));
+
+        public StudentItemControl()
+        {
+            InitializeComponent();
+            DataContextChanged += StudentItemControlDataContextChanged;
+        }
+
+        private void StudentItemControlDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var t = DataContext;
+            if (DataContext is Student)
+            {
+                //DataContext = this;
+            }
+        }
+
+        public Student Student
+        {
+            get => (Student)GetValue(StudentProperty);
+            set => SetValue(StudentProperty, value);
+        }
+
+        public ICommand RemoveStudentCommand
+        {
+            get => (ICommand)GetValue(RemoveStudentCommandProperty);
+            set => SetValue(RemoveStudentCommandProperty, value);
+        }
+    }
+}
